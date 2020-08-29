0508416bf12c4da3b633ab9c91dd00670903f11f
chaosknt <chaosknt@gmail.com>
2020-08-29 17:37:03 -0300

    Testiado ej6 tp 1

--- src/ar/edu/ort/tp1/ej06/Test.java
+++ src/ar/edu/ort/tp1/ej06/Test.java
@@ -8,24 +8,17 @@ public class Test {
 		
 		gastos.agregarGasto(Mes.ENERO, "Luz", 10);
 		gastos.agregarGasto(Mes.FEBRERO, "Luz", 10);
-		gastos.agregarGasto(Mes.MARZO, "Luz", 10);
-		
-		
-	
-		
+		gastos.agregarGasto(Mes.MARZO, "Luz", 10);			
+			
 		gastos.agregarGasto(Mes.ENERO, "Gas", 15);
 		gastos.agregarGasto(Mes.FEBRERO, "Gas", 15);
-		gastos.agregarGasto(Mes.MARZO, "Gas", 15);
-		
+		gastos.agregarGasto(Mes.MARZO, "Gas", 15);		
 		
 		gastos.agregarGasto(Mes.ENERO,"Agua", 9);
 		gastos.agregarGasto(Mes.FEBRERO,"Agua", 9);
-		gastos.agregarGasto(Mes.MARZO, "Agua", 9);
+		gastos.agregarGasto(Mes.MARZO, "Agua", 9);		
 		
-		
-		System.out.println("---");
-		
-		//double a [] [] = gastos.consolidarGastos();		
+		System.out.println("---");						
 		
 		System.out.println("Mostrando todo el gasto acumulado para el rubro Luz: "+gastos.gastoAcumulado("Luz"));
 		System.out.println("Mostrando todo el gasto acumulado para el rubro Sushi: "+gastos.gastoAcumulado("Sushi"));
